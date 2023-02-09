862f995d85293df1cc96b85cca7f9490ca97e797
azim0057 <ahmad.azim@tftus.com>
2023-02-09 18:03:00 +0530

    remove duplicates

---
+++ removeDuplicateFromSortedArray.js
@@ -0,0 +1,41 @@
+// 26: Remove Duplicate from sorted array
+// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+
+// Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+
+// Return k after placing the final result in the first k slots of nums.
+
+// Input: nums = [0,0,1,1,1,2,2,3,3,4]
+// Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+// Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+// It does not matter what you leave beyond the returned k (hence they are underscores).
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    for(let i=0;i<nums.length;){
+        if(nums[i] == nums[i+1]){
+            nums.splice(i,1)
+        }else{
+            i++
+        }
+    }
+    return nums.length
+};
+
+// Learnings
+
+// Array.splice()
+// array.splice(index, howmany, item1, ....., itemX)
+// Parameter 	Description
+// index ->	Required.// The position to add/remove items.// Negative value defines the position from the end of the array.
+// howmany ->	Optional.// Number of items to be removed.
+// item1, ..., itemX ->	Optional.// New elements(s) to be added.
+
+
+// Return Value
+// An array containing the removed items (if any).
+
+// Azim Ahmad
