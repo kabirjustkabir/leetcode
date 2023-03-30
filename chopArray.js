76f52e8d4d9e5f94443c165f568a390bede33fda
kabirjustkabir <aazim5215@gmail.com>
2023-03-31 00:03:38 +0530

    chopArray into given size with custom slice method

--- chopArray.js
+++ chopArray.js
@@ -5,16 +5,28 @@
 
 // Output:
 // [[1,2,3], [4,5,6], [7,8,9], [10]]
+const customSliceMethod = (arr,startIndex,endIndex)=>{
+    let sliceElements = []
+    for(let i=startIndex;i<endIndex;i++){
+        if(i < arr.length){
+            sliceElements.push(arr[i])
+        }else{
+            return sliceElements
+        }
+        
+    }
+    return sliceElements;
+}
 const chop = (arr,size)=>{
     let i=0;
     let output = []
     while(i<arr.length){
         console.log("tempArr",arr);
-        output.push(arr.slice(i,i+size))
+        output.push(customSliceMethod(arr,i,i+size))
         i += size;
     }
     return output
 }
 let arr= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-const result = chop(arr,4)
+const result = chop(arr,9)
 console.log(result);
